626788197952d57db455c17378a16deace81e2af
alexanderstahl93 <90091045+alexanderstahl93@users.noreply.github.com>
2023-02-27 11:26:23 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,26 @@
-function generatePitches() {
-    const persona = document.getElementById("persona").value;
-    const action = document.getElementById("action").value;
-    const description = document.getElementById("description").value;
-    
-    // Add your OpenAI API key here
-    const apiKey = "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";
-  
-    const prompt = `Please write 3 different compelling product pitches that speaks directly to the ${persona} and encourages them to ${action} on the ${description}. For each pitch, end with a tag line.`;
-    const numResponses = 3;
-    const length = 100;
-  
-    fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${apiKey}`
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        max_tokens: length,
-        n: numResponses,
-        stop: "."
-      })
-    })
-    .then(response => response.json())
-    .then(data => {
-      const pitches = data.choices.map(choice => choice.text.trim());
-  
-      // Display the pitches in the output section
-      document.getElementById("pitch1").querySelector(".content").textContent = pitches[0];
-      document.getElementById("pitch1").querySelector(".tagline").textContent = generateTagline();
-      document.getElementById("pitch2").querySelector(".content").textContent = pitches[1];
-      document.getElementById("pitch2").querySelector(".tagline").textContent = generateTagline();
-      document.getElementById("pitch3").querySelector(".content").textContent = pitches[2];
-      document.getElementById("pitch3").querySelector(".tagline").textContent = generateTagline();
-    })
-    .catch(error => console.error(error));
-  }
-  
+$(document).ready(function() {
+	$('#pitch-form').on('submit', function(event) {
+		event.preventDefault();
+		var persona = $('#persona').val();
+		var action = $('#action').val();
+		var description = $('#description').val();
+		$.ajax({
+			method: 'POST',
+			url: '/generate-pitches',
+			data: { persona: persona, action: action, description: description },
+			success: function(data) {
+				$('#pitch-container').html('');
+				$.each(data.pitches, function(index, pitch) {
+					var pitchElement = $('<div>').addClass('pitch');
+					var pitchText = $('<p>').text(pitch);
+					var tagline = $('<p>').addClass('tagline').text('"' + persona + '", "' + action + '", "' + description + '"');
+					pitchElement.append(pitchText).append(tagline);
+					$('#pitch-container').append(pitchElement);
+				});
+			},
+			error: function(xhr, status, error) {
+				console.error(error);
+			}
+		});
+	});
+});
--- server.js
+++ server.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const OpenAI = require('openai-api');
 
 const app = express();
+const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+const openai = new OpenAI(process.env.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);
 
 app.post('/generate-pitches', async (req, res) => {
-  const { persona, action, description } = req.body;
-  
+	try {
+		const persona = req.body.persona;
+		const action = req.body.action;
+		const description = req.body.description;
+		const prompt = `Write 3 different compelling product pitches that speak directly to the "${persona}" and encourage them to ${action} on the "${description}".`;
+		const completions = await openai.completions.create({
+			engine: 'davinci',
+			prompt: prompt,
+			max_tokens: 1024,
+			n: 3,
+			stop: ['\n', 'Pitch:', '1.', '2.', '3.']
+		});
+		const pitches = completions.choices.map(choice => choice.text.trim());
+		res.json({ pitches: pitches });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'An error occurred' });
+	}
+});
+
+app.listen(port, () => {
+	console.log(`App listening at http://localhost:${port}`);
+});
